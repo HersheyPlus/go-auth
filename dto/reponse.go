d7ec90d9fcd1508a77822a2b39e25103c849e2ad
Melbromss <melbromss@gmail.com>
2024-11-04 15:50:38 +0700

    setup

---
+++ dto/reponse.go
@@ -0,0 +1,18 @@
+package dto
+
+import ()
+
+type UserRegisterResponse struct {}
+
+type UserLoginResponse struct {}
+
+type UserUpdateResponse struct {}
+
+type UserGetProfileResponse struct {}
+
+type UserListResponse struct {}
+
+type UserSearchResponse struct {}
+
+type UserDeleteResponse struct {}
+
---
+++ dto/request.go
@@ -0,0 +1,19 @@
+package dto
+
+import ()
+
+
+type UserRegisterRequest struct {}
+
+type UserLoginRequest struct {}
+
+type UserUpdateRequest struct {}
+
+type UserDeleteRequest struct {}
+
+type UserGetProfileRequest struct {}
+
+type UserListRequest struct {}
+
+type UserSearchRequest struct {}
+
